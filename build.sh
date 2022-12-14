ac7982bbc9070218780260021b9e2922764a798a
rutp6vmp <rutp6vmp@yahoo.com.tw>
2022-12-14 09:13:00 +0800

    Initial commit

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 set -o errexit
 pip install --upgrade pip
+pip install django
+pip install django-mathfilters
+pip install django-simpleui
+pip install django-filter
+python -m pip install Pillow
+
 python manage.py migrate
 python manage.py superuser
 
